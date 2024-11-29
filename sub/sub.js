24b785c1908146cbdc18ed922f7b4219bd24f064
kms <128587805+kmssuper@users.noreply.github.com>
2024-11-29 10:11:38 +0900

    Update sub.js

--- sub/sub.js
+++ sub/sub.js
@@ -160,7 +160,7 @@ document.querySelectorAll('.matchup img').forEach(img => {
                     localStorage.setItem("winnerImage", winners[0].img);
 
                     // 우승자 페이지로 이동
-                    window.location.href = "winner.html";
+                    window.location.href = "./../winner.html";
                 } else {
                     // 라운드를 새로 시작하면서 매치업을 다시 생성
                     matchups = createMatchups(winners);
